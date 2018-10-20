5a1d2b53fe86d15233b5021757d2fc90cce934e5
igorfs10 <igorfs10@gmail.com>
2018-10-20 16:23:37 -0300

    Terminado o modal de contatos

--- cms/db.php
+++ cms/db.php
@@ -6,6 +6,12 @@
         return mysqli_query(conexaoDb(), $sql);
     }
 
+    function selectUsuarioBanco($id){
+        $sql = "SELECT * from tbl_contatos WHERE id=" . $id;
+        
+        return mysqli_query(conexaoDb(), $sql);
+    }
+
     function deleteBanco($id){
         $sql = "DELETE FROM tbl_contatos WHERE id= " . $id;
         mysqli_query(conexaoDb(), $sql);
--- cms/modal.php
+++ cms/modal.php
@@ -2,14 +2,25 @@
     include_once('db.php');
     
     $codigo = $_POST['idRegistro'];
-    $select = selectBanco();
+    $select = selectUsuarioBanco($codigo);
 
-    if($rs=mysqli_fetch_array($select))
-    {
+    if($rs=mysqli_fetch_array($select)){
         $nome = $rs["nome"];
         $telefone = $rs["telefone"];
         $celular = $rs["celular"];
         $email = $rs["email"];
+        $homepage = $rs["homepage"];
+        $facebook = $rs["facebook"];
+        $sugestao = $rs["sugestao"];
+        $informacao = $rs["informacao"];
+        $sexo = $rs["sexo"];
+        $profissao = $rs["profissao"];
+        
+        if($sexo == "m"){
+            $sexo = "Masculino";
+        }else{
+            $sexo = "Feminino";
+        }
     }
 ?>
 
@@ -17,7 +28,7 @@
     <head>
         <title>Modal</title>    
         <script src="js/jquery.js"></script>
-        
+        <link rel="stylesheet" type="text/css" href="css/style.css">
         <script>
             $(document).ready(function(){
                 //Function para fechar a modal
@@ -28,7 +39,7 @@
         </script>
     </head>
     <body>
-        <table width="98%" id="dadosContatos">
+        <table width="800px" height="500px" id="dadosContatos">
             <tr>
                 <td colspan="2" class="tituloDados">
                     Dados do Contato
@@ -37,26 +48,82 @@
             </tr>
             <tr>
                 <td class="linhaDados">
-                    Telefone:
+                    &ensp;&ensp;Telefone:
                 </td>
                 <td class="linhaDadosB">
                     <?php echo($telefone) ?>
                 </td>
             </tr>
             <tr>
-                <td>
-                    Celular:
+                <td class="linhaDados">
+                    &ensp;&ensp;Celular:
                 </td>
-                <td>
+                <td class="linhaDadosB">
                     <?php echo($celular) ?>
                 </td>
             </tr>
             <tr>
-                <td>
-                    Email:
+                <td class="linhaDados">
+                    &ensp;&ensp;Email:
                 </td>
-                <td>
+                <td class="linhaDadosB">
                     <?php echo($email) ?>
+                </td>
+            </tr>
+            <tr>
+                <td class="linhaDados">
+                    &ensp;&ensp;Homepage:
+                </td>
+                <td class="linhaDadosB">
+                    <?php echo($homepage) ?>
+                </td>
+            </tr>
+            <tr>
+                <td class="linhaDados">
+                    &ensp;&ensp;Facebook:
+                </td>
+                <td class="linhaDadosB">
+                    <?php echo($facebook) ?>
+                </td>
+            </tr>
+            <tr>
+                <td class="linhaDados">
+                    &ensp;&ensp;Sexo:
+                </td>
+                <td class="linhaDadosB">
+                    <?php echo($sexo) ?>
+                </td>
+            </tr>
+            <tr>
+                <td class="linhaDados">
+                    &ensp;&ensp;Profissão:
+                </td>
+                <td class="linhaDadosB">
+                    <?php echo($profissao) ?>
+                </td>
+            </tr>
+            <tr>
+                <td class="linhaDadosTexto  " colspan="2">
+                    Sugestão:
+                </td>
+            </tr>
+            <tr>
+                <td class="linhaDadosTexto" colspan="2">
+                    <textarea rows="4" cols="60" disabled>
+                        <?php echo($sugestao) ?>
+                    </textarea>
+                </td>
+            </tr>
+            <tr>
+                <td class="linhaDadosTexto" colspan="2">
+                    Informação:
+                </td>
+            </tr>
+            <tr>
+                <td class="linhaDadosTexto" colspan="2">
+                    <textarea rows="4" cols="60" disabled>
+                        <?php echo($informacao) ?>
+                    </textarea>
                 </td>
             </tr>
         </table>
